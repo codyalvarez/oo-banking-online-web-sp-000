8313c69ce272e1fe4404e323ad4ba940790a0343
Cody Alvarez <cody_alvarez19@outlook.com>
2021-03-31 02:20:49 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -18,7 +18,7 @@ class BankAccount
     self.balance -= amount
   end
 
->>>>>>> 113d5c8af93ec93b025344f87ad68eb16b14d10d
+# >>>>>>> 113d5c8af93ec93b025344f87ad68eb16b14d10d
   def display_balance
     "Your balance is $#{balance}."
   end
